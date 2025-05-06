3a5341e8c4e6429e9ba167535cf0c15c45e692de
Adnan-Ahmed07 <masudrana52443@gmail.com>
2025-05-06 15:29:13 -0600

    photo issues are

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -27,13 +27,12 @@ export const photos: Photo[] = [
     date: "April 2, 2023",
   },
   {
-    url: "/images/Photo3.jpeg",
+    url: "/images/photo3.jpg",
     caption: "Celebrating our anniversary",
-  
     date: "April 15, 2023",
   },
   {
-    url: "/images/Photo4.jpeg",
+    url: "/images/photo4.jpg",
     caption: "Group photo with our closest friends",
     date: "May 1, 2023",
   },
